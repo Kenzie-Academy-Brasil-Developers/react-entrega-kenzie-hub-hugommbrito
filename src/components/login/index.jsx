26e1b9c73f93a48e3d502295e932c45b5d008c3e
Hugo Brito <hugomdmb@gmail.com>
2022-12-05 00:40:49 -0300

    style: Create login page styles

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,17 +1,38 @@
+import { useForm } from 'react-hook-form'
+
 import { api } from "../../services/api";
-import { StyledLogin } from "./style"
-import { StyledTitle } from "../../styles/fonts"
+
+import { StyledLogin, StyledLoginHeader } from "./style"
+import { StyledTitle, StyledHeadline } from "../../styles/fonts"
+import { StyledInput } from "../../styles/inputs";
+import { StyledButton } from "../../styles/buttons";
 
 export const LoginPage = () => {
-    const APIRequest = async () => {
-        const response = await api.get('users')
-        console.log(response.data);
-    }
-    APIRequest()
+    const { register, handleSubmit } = useForm()
 
     return(
-        <StyledLogin >
-            <StyledTitle >login</StyledTitle>
+        <>
+        <StyledLoginHeader className="container-form">
+            <StyledTitle type='title1' color='primary'>Kenzie Hub</StyledTitle>
+        </StyledLoginHeader>
+        <StyledLogin className="container-form">
+            <StyledTitle >Login</StyledTitle>
+            <form >
+                <div className="inputDiv">
+                    <StyledHeadline size='9' >Email</StyledHeadline>
+                    <StyledInput placeholder='Digite seu Email aqui' {...register('email')}/>
+                    {/* { errors.email && <StyledHeadline color='primary' >{errors.email.message}</StyledHeadline>} */}
+                </div>
+                <div className="inputDiv">
+                    <StyledHeadline size='9' >Senha</StyledHeadline>
+                    <StyledInput type='password' placeholder='Digite sua senha aqui' {...register('password')} />
+                    {/* { errors.password && <StyledHeadline color='primary' >{errors.password.message}</StyledHeadline>} */}
+                </div>
+                <StyledButton >Entrar</StyledButton>
+            </form>
+            <StyledHeadline size='9.6' bold color='dark' >Ainda não possui uma conta?</StyledHeadline>
+            <StyledButton disabled >Cadastre-se</StyledButton>
         </StyledLogin>
+        </>
     )
 }
